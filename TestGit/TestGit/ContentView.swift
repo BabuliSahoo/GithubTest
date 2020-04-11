6448d90f648bf4487c556a5204b63f02b04d0896
Babuli Sahoo <babuli.sahoo4@gmail.com>
2020-04-11 12:03:18 +0530

    Initial Project Added

---
+++ TestGit/TestGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestGit
+//
+//  Created by Babuli Sahoo on 11/04/20.
+//  Copyright © 2020 Babuli Sahoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
